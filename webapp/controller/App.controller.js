452131ba4c3b481ce322f5505aecf929d101183d
Karl Keller <kgk86@hotmail.com>
2025-01-12 09:28:32 +0100

    Add onInit() function to the controller

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,24 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast"
+  "sap/m/MessageToast",
+  "sap/ui/model/json/JSONModel"
 ], (Controller, MessageToast) => {
   "use strict";
 
   return Controller.extend("ui5.walkthrough.controller.App", {
      onShowHello() {
         MessageToast.show("Hello World");
+     },
+
+     inInit() {
+      // set O Data model on view
+      const oData = {
+        recipient : {
+          name : "World"
+        }
+      };
+      const oModel = new JSONModel(oData);
+      this.getView().setModel(oMedel);
      }
   });
 });
