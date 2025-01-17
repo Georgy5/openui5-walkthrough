e350ceb335dafa78445d9c4f69fdebc5aa9b50b6
Karl Keller <kgk86@hotmail.com>
2025-01-17 08:29:33 +0100

    Move init function and UI assets from the App controller to the Component

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,35 +1,17 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast",
-  "sap/ui/model/json/JSONModel",
-  "sap/ui/model/resource/ResourceModel"
-], (Controller, MessageToast, JSONModel, ResourceModel) => {
+  "sap/m/MessageToast"
+], (Controller, MessageToast) => {
   "use strict";
 
   return Controller.extend("ui5.walkthrough.controller.App", {
-     onShowHello() {
-        // read message from i18n model
-        const oBundle = this.getView().getModel("i18n").getResourceBundle();
-        const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+    onShowHello() {
+      // read message from i18n model
+      const oBundle = this.getView().getModel("i18n").getResourceBundle();
+      const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+      const sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
-        MessageToast.show(sMsg);
-     },
-
-     onInit() {
-      // set O Data model on view
-      const oData = {
-        recipient : {
-          name : "World"
-        }
-      };
-      const oModel = new JSONModel(oData);
-      this.getView().setModel(oModel);
-
-      const i18nModel = new ResourceModel({
-        bundleName: "ui5.walkthrough.i18n.i18n"
-      });
-      this.getView().setModel(i18nModel, "i18n")
-     }
+      MessageToast.show(sMsg);
+    }
   });
 });
