ae0d6aaf65edf47f3f2f8b5e7692f2631a89a03f
Karl Keller <kgk86@hotmail.com>
2025-03-13 13:41:53 +0100

    Move 'onShowHello' method from the app controller to its HelloPanel controller.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,14 +4,5 @@ sap.ui.define([
 ], (Controller, MessageToast) => {
   "use strict";
 
-  return Controller.extend("ui5.walkthrough.controller.App", {
-    onShowHello() {
-      // read message from i18n model
-      const oBundle = this.getView().getModel("i18n").getResourceBundle();
-      const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-      const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-      MessageToast.show(sMsg);
-    }
-  });
+  return Controller.extend("ui5.walkthrough.controller.App", {});
 });
